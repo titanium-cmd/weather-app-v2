0b95c89ef1560106568c6ff1dc5710191a090cd4
titanium-cmd <nkansahisaac41@gmail.com>
2022-10-25 09:36:18 +0000

    init commit

---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:weather_app_v2/views/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        const Duration(seconds: 5),
+        () =>
+            {Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()))});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff38C172),
+      body: Center(
+        child: Image.asset('assets/weather_logo.png'),
+      ),
+    );
+  }
+}
